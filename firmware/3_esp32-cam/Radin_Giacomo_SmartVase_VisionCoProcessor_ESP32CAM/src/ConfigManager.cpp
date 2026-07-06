dbb288358ec3ceeddeea4a81f7c3a409759dc174
antpss <antoniopossemato@gmail.com>
2026-07-06 10:21:09 +0200

    fix: configmanager upload time set at 3600

--- firmware/3_esp32-cam/Radin_Giacomo_SmartVase_VisionCoProcessor_ESP32CAM/src/ConfigManager.cpp
+++ firmware/3_esp32-cam/Radin_Giacomo_SmartVase_VisionCoProcessor_ESP32CAM/src/ConfigManager.cpp
@@ -13,40 +13,37 @@ void makeDeviceId() {
 
 void loadConfig() {
     prefs.begin("cam", true);
-    cfg.wifi_ssid           = prefs.getString("wifi_ssid",           "");
-    cfg.wifi_pass           = prefs.getString("wifi_pass",           "");
+#ifdef SMARTVASE_CAM_SECRETS_H
+    cfg.wifi_ssid           = prefs.getString("wifi_ssid",           SECRET_WIFI_SSID);
+    cfg.wifi_pass           = prefs.getString("wifi_pass",           SECRET_WIFI_PASS);
+    cfg.firebase_api_key    = prefs.getString("fb_api_key",          SECRET_FIREBASE_API_KEY);
+    cfg.firebase_project_id = prefs.getString("fb_proj_id",          SECRET_FIREBASE_PROJECT_ID);
+    cfg.firebase_email      = prefs.getString("fb_email",            SECRET_FIREBASE_EMAIL);
+    cfg.firebase_password   = prefs.getString("fb_pass",             SECRET_FIREBASE_PASSWORD);
+#else
+    cfg.wifi_ssid           = prefs.getString("wifi_ssid",           "XXL");
+    cfg.wifi_pass           = prefs.getString("wifi_pass",           "pomodoro");
+    cfg.firebase_api_key    = prefs.getString("fb_api_key",          "");
+    cfg.firebase_project_id = prefs.getString("fb_proj_id",          "");
+    cfg.firebase_email      = prefs.getString("fb_email",            "");
+    cfg.firebase_password   = prefs.getString("fb_pass",             "");
+#endif
     cfg.upload_url          = prefs.getString("upload_url",          "");
     cfg.interval_s          = prefs.getUInt  ("interval_s",          300);
-    cfg.firebase_api_key    = prefs.getString("firebase_api_key",    "");
-    cfg.firebase_project_id = prefs.getString("firebase_project_id", "");
-    cfg.firebase_email      = prefs.getString("firebase_email",      "");
-    cfg.firebase_password   = prefs.getString("firebase_password",   "");
     cfg.roi_center_x        = prefs.getUInt  ("roi_cx",              400);
     cfg.roi_center_y        = prefs.getUInt  ("roi_cy",              300);
     cfg.roi_radius          = prefs.getUInt  ("roi_r",               280);
     prefs.end();
-
-#ifdef SMARTVASE_CAM_SECRETS_H
-    cfg.wifi_ssid           = SECRET_WIFI_SSID;
-    cfg.wifi_pass           = SECRET_WIFI_PASS;
-    cfg.firebase_api_key    = SECRET_FIREBASE_API_KEY;
-    cfg.firebase_project_id = SECRET_FIREBASE_PROJECT_ID;
-    cfg.firebase_email      = SECRET_FIREBASE_EMAIL;
-    cfg.firebase_password   = SECRET_FIREBASE_PASSWORD;
-#else
-    cfg.wifi_ssid   = "XXL";
-    cfg.wifi_pass   = "pomodoro";
-#endif
 }
 
 void saveConfig() {
     prefs.begin("cam", false);
     prefs.putString("wifi_ssid",           cfg.wifi_ssid);
     prefs.putString("wifi_pass",           cfg.wifi_pass);
-    prefs.putString("firebase_api_key",    cfg.firebase_api_key);
-    prefs.putString("firebase_project_id", cfg.firebase_project_id);
-    prefs.putString("firebase_email",      cfg.firebase_email);
-    prefs.putString("firebase_password",   cfg.firebase_password);
+    prefs.putString("fb_api_key",          cfg.firebase_api_key);
+    prefs.putString("fb_proj_id",          cfg.firebase_project_id);
+    prefs.putString("fb_email",            cfg.firebase_email);
+    prefs.putString("fb_pass",             cfg.firebase_password);
     prefs.putString("upload_url",          cfg.upload_url);
     prefs.putUInt  ("interval_s",          cfg.interval_s);
     prefs.putUInt  ("roi_cx",              cfg.roi_center_x);
